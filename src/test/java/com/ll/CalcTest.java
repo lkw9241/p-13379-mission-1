5aded05b45e93a7460a5165dbefd7acea13e29c4
jhs512 <jangka512@gmail.com>
2024-11-26 12:00:47 +0900

    work

--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -13,4 +13,12 @@ public class CalcTest {
 
         assertThat(rs).isEqualTo(8);
     }
+
+    @Test
+    @DisplayName("10 + 5")
+    public void t2() {
+        int rs = Calc.run("10 + 5");
+
+        assertThat(rs).isEqualTo(15);
+    }
 }
