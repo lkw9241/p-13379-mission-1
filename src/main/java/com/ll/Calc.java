bc092b6554b1911bf197408b8128ab1ac5403ec1
jhs512 <jangka512@gmail.com>
2024-12-06 14:25:49 +0900

    10 * 10 == 100, g

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -4,6 +4,17 @@ import java.util.Arrays;
 
 public class Calc {
     public static int run(String expr) {
+        if (expr.contains(" * ")) {
+            String[] exprBits = expr.split(" \\* ");
+
+            int product = Arrays.stream(exprBits)
+                    .map(Integer::parseInt)
+                    .reduce((a, b) -> a * b)
+                    .orElse(0);
+
+            return product;
+        }
+
         expr = expr.replaceAll(" - ", " + -");
 
         String[] exprBits = expr.split(" \\+ ");
