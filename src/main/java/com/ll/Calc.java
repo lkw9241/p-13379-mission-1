1dd136b034affbaa3e79dfd89592db38c2924457
jhs512 <jangka512@gmail.com>
2024-12-06 14:15:30 +0900

    2 - 1 == 1, b

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,20 +2,13 @@ package com.ll;
 
 public class Calc {
     public static int run(String expr) {
-        if (expr.contains(" + ")) {
-            String[] exprBits = expr.split(" \\+ ");
+        expr = expr.replaceAll(" - ", " + -");
 
-            int num1 = Integer.parseInt(exprBits[0]);
-            int num2 = Integer.parseInt(exprBits[1]);
-
-            return num1 + num2;
-        }
-
-        String[] exprBits = expr.split(" \\- ");
+        String[] exprBits = expr.split(" \\+ ");
 
         int num1 = Integer.parseInt(exprBits[0]);
         int num2 = Integer.parseInt(exprBits[1]);
 
-        return num1 - num2;
+        return num1 + num2;
     }
 }
