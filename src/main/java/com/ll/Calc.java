09f9e7df6d0ff6e6427784020da8dbca6baf336c
jhs512 <jangka512@gmail.com>
2024-12-06 14:51:12 +0900

    (20 + 20) + 20 == 60, g

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -4,7 +4,20 @@ import java.util.Arrays;
 
 public class Calc {
     public static int run(String expr) {
+        expr = expr.trim();
         expr = removeUnnecessaryBrackets(expr);
+        expr = expr.replaceAll(" - ", " + -");
+
+        if (expr.contains("(")) {
+            String[] exprBits = splitTwoParts(expr);
+
+            int sum = Arrays.stream(exprBits)
+                    .map(Calc::run)
+                    .reduce((a, b) -> a + b)
+                    .orElse(0);
+
+            return sum;
+        }
 
         if (expr.contains(" * ") && expr.contains(" + ")) {
             String[] exprBits = expr.split(" \\+ ");
@@ -28,8 +41,6 @@ public class Calc {
             return product;
         }
 
-        expr = expr.replaceAll(" - ", " + -");
-
         String[] exprBits = expr.split(" \\+ ");
 
         int sum = Arrays.stream(exprBits)
@@ -38,6 +49,24 @@ public class Calc {
                 .orElse(0);
 
         return sum;
+    }
+
+    private static String[] splitTwoParts(String expr) {
+        int bracketDepth = 0;
+
+        for (int i = 0; i < expr.length(); i++) {
+            char c = expr.charAt(i);
+
+            if (bracketDepth == 0 && c == '+') {
+                return new String[]{expr.substring(0, i), expr.substring(i + 1)};
+            } else if (c == '(') {
+                bracketDepth++;
+            } else if (c == ')') {
+                bracketDepth--;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid expression: " + expr);
     }
 
     private static String removeUnnecessaryBrackets(String expr) {
